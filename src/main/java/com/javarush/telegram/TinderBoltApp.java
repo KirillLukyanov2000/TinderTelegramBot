1dfb6283de397f327dfda07f8a4c000ca9c6f4c3
Kirill Lukyanov <kirs2000@mail.ru>
2024-05-29 10:23:49 +0300

    date and message mode added

--- src/main/java/com/javarush/telegram/TinderBoltApp.java
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -18,6 +18,7 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
 
     private ChatGPTService chatGPT = new ChatGPTService(OPEN_AI_TOKEN);
     private DialogMode currentMode = null;
+    private ArrayList<String> messageList = new ArrayList<>();
 
     public TinderBoltApp() {
         super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
@@ -26,11 +27,12 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
     @Override
     public void onUpdateEventReceived(Update update) {
         String message = getMessageText();
+
+        //START MODE
         if (message.equals("/start")) {
             currentMode = DialogMode.MAIN;
             sendPhotoMessage("main");
             sendTextMessage(loadMessage("main"));
-
             showMainMenu("главное меню бота", "/start"
                     , "генерация Tinder-профиля \uD83D\uDE0E", "/profile"
                     , "сообщение для знакомства \uD83E\uDD70", "/opener"
@@ -39,6 +41,8 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
                     , "задать вопрос чату GPT \uD83E\uDDE0", "/gpt");
             return;
         }
+
+        //GPT MODE
         if (message.equals("/gpt")) {
             currentMode = DialogMode.GPT;
             sendPhotoMessage("gpt");
@@ -47,16 +51,63 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
         }
         if (currentMode == DialogMode.GPT) {
             String prompt = loadPrompt("gpt");
+            Message serviceMessage = sendTextMessage("Chat GPT is updating info...");
             String answer = chatGPT.sendMessage(prompt, message);
-            sendTextMessage(answer);
+            updateTextMessage(serviceMessage, answer);
             return;
         }
-        sendTextMessage("*Hello!*");
-        String message2 = getMessageText();
-        sendTextButtonsMessage("_You wrote:_ " + message2);
-        sendTextButtonsMessage("Choose button: ", "Start", "start"
-                , "Stop", "stop");
 
+        //DATE MODE
+        if (message.equals("/date")) {
+            currentMode = DialogMode.DATE;
+            sendPhotoMessage("date");
+
+            sendTextButtonsMessage(loadMessage("date"),
+                    "Ariana Grande", "date_grande"
+                    , "Margo Robbi", "date_robbie"
+                    , "Zendaya", "date_zendaya"
+                    , "Ryan Gosling", "date_gosling"
+                    , "Tom Hardy", "date_hardy");
+            return;
+        }
+        if (currentMode == DialogMode.DATE) {
+            String query = getCallbackQueryButtonKey();
+            if (query.startsWith("date_")) {
+                sendPhotoMessage(query);
+                sendTextMessage("Good choice! Try to appoint a date in 5 messages ❤\uFE0F");
+                String prompt = loadPrompt(query);
+                chatGPT.setPrompt(prompt);
+                return;
+            }
+            Message serviceMessage = sendTextMessage("Waiting an answer...");
+            String answer = chatGPT.addMessage(message);
+            updateTextMessage(serviceMessage, answer);
+            return;
+        }
+
+        //MESSAGE MODE
+        if (message.equals("/message")) {
+            currentMode = DialogMode.MESSAGE;
+            sendPhotoMessage("message");
+            sendTextButtonsMessage(loadMessage("message"),
+                    "Next message", "message_next"
+                    , "Invite on a date", "message_date"
+            );
+            return;
+        }
+        if (currentMode == DialogMode.MESSAGE) {
+            String query = getCallbackQueryButtonKey();
+            if (query.startsWith("message_")) {
+                String chatHistory = String.join("\n\n", messageList);
+                Message serviceMessage = sendTextMessage("Chat GPT is updating info...");
+                String answer = chatGPT.sendMessage(loadPrompt(query), chatHistory);
+                updateTextMessage(serviceMessage, answer);
+                return;
+            }
+            messageList.add(message);
+            return;
+
+        }
 
     }
 
